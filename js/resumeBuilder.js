7c147259797dc3e549be2405bf0677c1b1c3148d
Valerie Mettler <valeriemettler@gmail.com>
2015-02-23 10:44:45 -0800

    first commit

---
+++ js/resumeBuilder.js
@@ -0,0 +1,66 @@
+//JSON
+
+var bio = {
+    "name": "Valerie Mettler",
+    "role": "Web Developer",
+    "contacts": {
+        "email": "valeriemettler@gmail.com",
+        "github": "github.com/valeriemettler",
+        "twitter": "@valmett",
+        "location": "The Bay Area"
+    },
+    "picture": "images/val.jpg",
+    "welcomeMessage": "Welcome to my Resume Website!",
+    "skills": ["Javascript, Ruby, Rails, etc"]
+};
+
+var work = {
+    "jobs": [
+        {
+            "employer": "NYC Department of Education",
+            "title": "English Teacher",
+            "location": "NY, NY",
+            "dates": "2008-2014",
+            "description": "Taught ESL in a bilingual middle school",
+        }
+    ]
+};
+
+var projects = {
+    "projects": [
+        {
+            "title": "LanguageCheck",
+            "dates": "2014",
+            "description": "Get help and help others with short language translations",
+            "images": [
+                "images/languagecheck.gif"
+            ],
+        }
+    ]
+};
+
+var eduction = {
+    "schools": [
+        {
+            "name": "McGill University",
+            "location": "Montreal, Quebec, Canada",
+            "degree": "Master of Arts",
+            "majors": [
+                "Second Language Education",
+                "Spanish",
+                "French"
+            ],
+            "dates": 2004,
+            "url": "www.mcgill.ca"
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "Full Stack Web Development Course",
+            "school": "Bloc.io",
+            "date": "2014",
+            "url": "bloc.io",
+        }
+    ]
+};
+
